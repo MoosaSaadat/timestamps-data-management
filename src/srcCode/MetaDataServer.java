3fbc95228838b04c09fe0ff9e849281507fa7c46
Muhammad Musa <moosasaadat99@gmail.com>
2020-06-07 00:56:04 +0500

    Complete sendMetaData()

--- src/srcCode/MetaDataServer.java
+++ src/srcCode/MetaDataServer.java
@@ -2,11 +2,18 @@ package srcCode;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Random;
 
 public class MetaDataServer extends Thread {
 	private class RequestHandler extends Thread {
@@ -17,12 +24,67 @@ public class MetaDataServer extends Thread {
 		public RequestHandler(Socket socket) {
 			this.socket = socket;
 		}
+		
+		private int getModificationBit(File file, String clientID) throws IOException {
+
+			// Read previous timestamp and discard it
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+			ois.readLong();
+			
+			// Read neighbors
+			HashMap<String, Integer> neighbors = null;
+			try {
+				neighbors = (HashMap<String, Integer>) ois.readObject();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			ois.close();
+
+			// Return Modified Bit for the given clientID
+			return neighbors.get(clientID);
+
+		}
+		
+		private long getTimestamp(File file, String clientID) throws IOException {
+
+			// Read previous timestamp
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+			long timestamp = ois.readLong();
+			ois.close();
+			
+			return timestamp;
+			
+		}
 
 		private void sendMetaData(PrintWriter printer, String clientID) {
 			// TODO: Send meta data of local files to client using "modified bit" approach.
 			// That is, send meta data of all files that were modified since
 			// the client downloaded the file the last time or that are new (= have never
 			// been downloaded by this client so far)
+			
+			// Search all metadata files
+			File[] files = rootDir.listFiles();
+			for (int i = 0; i < files.length; i++) {
+				File file = files[i];
+				if (file.getPath().endsWith(GlobalConstants.MetaDataFileSuffix)) {
+					int modificationBit = 0;
+					long modificationTimestamp = 0;
+					try {
+						modificationBit = getModificationBit(file, clientID);
+						if (modificationBit == 1)
+							modificationTimestamp = getTimestamp(file, clientID);						
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					
+					// Send filename and timestamp if it has been modified
+					if (modificationBit == 1) {
+						printer.print(file.getName().replace(GlobalConstants.MetaDataFileSuffix, ""));
+						printer.print(modificationTimestamp);
+					}
+				}
+			}
+
 
 		}
 
